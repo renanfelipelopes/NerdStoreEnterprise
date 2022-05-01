cf5b42a625bf2a843229ac0ab99ed6985b0a3cb4
renanfelipelopes <renanlopes.ads@gmail.com>
2022-04-30 22:51:01 -0300

    Criando MainController

---
+++ src/services/NSE.Identidade.API/Controllers/MainController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : Controller
+    {
+        protected ICollection<string> Erros = new List<string>();
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !Erros.Any();
+        }
+    }
+}
