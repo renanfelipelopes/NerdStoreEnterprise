a1c9357e9052168726d9c5e28d2c32ed9494a19f
renanfelipelopes <renanlopes.ads@gmail.com>
2022-05-03 22:38:15 -0300

    Removendo comentarios

--- src/services/NSE.Identidade.API/Startup.cs
+++ src/services/NSE.Identidade.API/Startup.cs
@@ -26,7 +26,6 @@ namespace NSE.Identidade.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddIdentityConfiguration(Configuration);
@@ -38,7 +37,6 @@ namespace NSE.Identidade.API
             services.AddSwaggerConfiguration();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseSwaggerConfiguration();
