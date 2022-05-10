41f2f852d5555a336f838d87289f708fe585a99b
renanfelipelopes <renanlopes.ads@gmail.com>
2022-05-09 21:57:58 -0300

    Implementando consumo de API externa via HttpService (Login)

---
+++ src/web/NSE.WebApp.MVC/Service/AutenticacaoService.cs
@@ -0,0 +1,38 @@
+﻿using NSE.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Service
+{
+    public class AutenticacaoService : IAutenticacaoService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AutenticacaoService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> Login(UsuarioLogin usuarioLogin)
+        {
+            var loginContent = new StringContent(
+                JsonSerializer.Serialize(usuarioLogin),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _httpClient.PostAsync("https://localhost:44320​/api​/identidade​/autenticar", loginContent);
+
+            return JsonSerializer.Deserialize<string>(await response.Content.ReadAsStringAsync());
+        }
+
+        public async Task<string> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Service/IAutenticacaoService.cs
@@ -0,0 +1,12 @@
+﻿using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Service
+{
+    public interface IAutenticacaoService
+    {
+        Task<string> Login(UsuarioLogin usuarioLogin);
+
+        Task<string> Registro(UsuarioRegistro usuarioRegistro);
+    }
+}
