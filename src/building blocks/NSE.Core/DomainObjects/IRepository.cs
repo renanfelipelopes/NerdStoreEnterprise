4af0e2dc81985937b19806cc2787d06a0d976cd0
renanfelipelopes <renanlopes.ads@gmail.com>
2022-07-05 14:20:42 -0300

    Criando Migrations e implementando IRepository

---
+++ src/building blocks/NSE.Core/DomainObjects/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace NSE.Core.DomainObjects
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+    }
+}
+ 
---
+++ src/services/NSE.Catalogo.API/Models/IProdutoRepository.cs
@@ -0,0 +1,9 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Catalogo.API.Models
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+    }
+}
+ 
