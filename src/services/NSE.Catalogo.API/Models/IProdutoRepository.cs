1d1553e6245945b0ed40aae6bfc413a93e8ed4a3
renanfelipelopes <renanlopes.ads@gmail.com>
2022-07-06 22:24:23 -0300

    Implementando Interface de ProdutoRepository

--- src/services/NSE.Catalogo.API/Models/IProdutoRepository.cs
+++ src/services/NSE.Catalogo.API/Models/IProdutoRepository.cs
@@ -1,9 +1,16 @@
 ﻿using NSE.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace NSE.Catalogo.API.Models
 {
     public interface IProdutoRepository : IRepository<Produto>
     {
+        Task<IEnumerable<Produto>> ObterTodos();
+        Task<Produto> ObterPorId(Guid Id);
+
+        void Adicionar(Produto produto);
+        void Atualizar(Produto produto);
     }
 }
- 
