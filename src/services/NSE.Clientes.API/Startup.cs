d32239ecc24a94516a6bc2e0072910435106e18f
renanfelipelopes <renanlopes.ads@gmail.com>
2022-07-17 08:20:34 -0300

    Implentando configuracoes

--- src/services/NSE.Clientes.API/Startup.cs
+++ src/services/NSE.Clientes.API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSE.Clientes.API.Configuration;
 using NSE.WebAPI.Core.Identidade;
 
 namespace NSE.Clientes.API
@@ -30,13 +31,22 @@ namespace NSE.Clientes.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApiConfiguration(Configuration);
+
             services.AddJwtConfiguration(Configuration);
 
+            services.AddSwaggerConfiguration();
+
             services.AddMediatR(typeof(Startup));
+
+            services.RegisterServices();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwaggerConfiguration();
+
+            app.UseApiConfiguration(env);
         }
     }
 }
