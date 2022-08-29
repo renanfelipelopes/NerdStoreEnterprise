682d57ae6c08a30f5fe0b4770c65d47ad8118198
renanfelipelopes <renanlopes.ads@gmail.com>
2022-08-29 20:10:54 -0300

    Criando projeto NSE.Pagamentos.NerdsPag

---
+++ NSE.Pagamentos.NerdsPag/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSE.Pagamentos.NerdsPag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
